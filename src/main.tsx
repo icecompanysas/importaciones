df636ee7d63ded527c16bb0493a4785306e61b2b
icecompanysas <54846370+icecompanysas@users.noreply.github.com>
2024-06-11 14:19:08 -0500

    arreglos en importaciones

--- src/main.tsx
+++ src/main.tsx
@@ -1,11 +1,10 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
-import App from './App.tsx'
-import './index.css'
+import ReactDOM from 'react-dom/client'; // Asegúrate de importar desde 'react-dom/client'
+import App from './App.tsx';
+import './index.css';
 import { BrowserRouter } from 'react-router-dom';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
   </BrowserRouter>,
-)
+);
